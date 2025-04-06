ff0955367d5a4466a6e957467563de1bee82397f
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2025-04-06 20:03:38 +0300

    add and set up tailwind

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { persistor, store } from "./redux/store";
 import App from "./App";
 import { PersistGate } from "redux-persist/integration/react";
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
